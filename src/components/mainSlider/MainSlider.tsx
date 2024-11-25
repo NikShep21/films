789c49f46f3e354ae8354ebcbd991c834d01251f
Lev Klimenko <you@example.com>
2024-11-25 16:12:09 +0800

    ohhhh, back the past

--- src/components/mainSlider/MainSlider.tsx
+++ src/components/mainSlider/MainSlider.tsx
@@ -1,75 +1,65 @@
-import { useEffect, useState } from "react";
-import styles from "./MainSlider.module.scss";
+'use client'
+import styles from './MainSlider.module.scss'
 import { getMovieMassive } from "@/api/response";
 import useResponse from "@/hooks/useResponse";
-import BigCard from "../bigCard/BigCard";
-import ButtonSlider from "../ui/buttonSlider/ButtonSlider";
+import BigCard from '../bigCard/BigCard';
+import { useEffect, useState } from 'react';
+import ButtonSlider from '../ui/buttonSlider/ButtonSlider';
+interface SliderType{
+    typeFilms: "now_playing" | "popular" | "top_rated" | "upcoming",
 
-interface SliderType {
-  typeFilms: "now_playing" | "popular" | "top_rated" | "upcoming";
 }
 
-const Slider = ({ typeFilms }: SliderType) => {
-  const [isLoad, errors, data] = useResponse(getMovieMassive, typeFilms);
-  const [countActiveSlider, setCountActiveSlider] = useState(2);
-  const [sliderData, setSliderData] = useState([]);
-
-  const SLIDES_TO_SHOW = 5;
-  const SLIDE_WIDTH = 940;
-
-  // Подготовка индекса с учетом цикличности
-  const prepareIndex = (index: number) => {
-    if (index >= data.length) return index - data.length;
-    if (index < 0) return index + data.length;
-    return index;
-  };
-
-  // Генерация массива для отображения
-  const generateSliderData = (activeIndex: number) => {
-    const slides = [];
-    for (let i = -2; i <= 2; i++) {
-      slides.push(data[prepareIndex(activeIndex + i)]);
+const Slider = ({typeFilms}:SliderType) => {
+    const [isLoad, errors, data] = useResponse(getMovieMassive, typeFilms);
+    const [sliderData, setSliderData] = useState([])
+    console.log(sliderData)
+    const [countActiveSlider, setCountActiveSlider] = useState(2)
+    const [offset, setOffset] = useState<number>(-1880)
+    function prepareCount(count:number){
+      if (count > 20){
+        return count - 20
+      }
+      if (count < 0){
+        return count + 20
+      }
+      else{
+        return count
+      }
     }
-    return slides;
-  };
-
-  // Движение влево
-  const moveLeft = () => {
-    const newActiveIndex = prepareIndex(countActiveSlider - 1);
-    setCountActiveSlider(newActiveIndex);
-    setSliderData(generateSliderData(newActiveIndex));
-  };
-
-  // Движение вправо
-  const moveRight = () => {
-    const newActiveIndex = prepareIndex(countActiveSlider + 1);
-    setCountActiveSlider(newActiveIndex);
-    setSliderData(generateSliderData(newActiveIndex));
-  };
-
-  // Инициализация данных
-  useEffect(() => {
-    if (!isLoad) {
-      setSliderData(generateSliderData(countActiveSlider));
+    function moveLeft(){
+      setOffset(offset+940)
+      setCountActiveSlider(countActiveSlider-1)
+      setSliderData(sliderData.slice(0,-1))
+      setSliderData([data[prepareCount(countActiveSlider-3)], ...sliderData])
     }
-  }, [isLoad, countActiveSlider]);
-
+    function moveRight(){
+      setOffset(offset-940)
+      setCountActiveSlider(countActiveSlider+1)
+      setSliderData(sliderData.slice(1))
+      setSliderData([...sliderData, data[prepareCount(countActiveSlider+3)]])
+    }
+    useEffect(()=>{
+      if (!isLoad){
+        setSliderData(data.slice(0,5))
+      }
+    },[isLoad])
+    console.log(sliderData)
   return (
     <div className={styles.slider}>
-      <ButtonSlider onClick={moveLeft} size="70px" className={styles.arrowLeft} type="left" />
-      <div
-        className={styles.sliderLine}
-        style={{
-          transform: `translateX(${-SLIDE_WIDTH}px)`,
-        }}
-      >
-        {sliderData.map((elem, index) => (
-          <BigCard key={index} card={elem} />
-        ))}
+      <ButtonSlider onClick={moveLeft} size='70px' className={styles.arrowLeft}  type='left'/>
+      <div style = {{left: offset}} className={styles.sliderLine}>
+        {
+          sliderData.map((elem)=>{
+            return(
+              <BigCard  card={elem}></BigCard>
+            )
+          })
+        }
       </div>
-      <ButtonSlider onClick={moveRight} size="70px" className={styles.arrowRight} type="right" />
+      <ButtonSlider onClick={moveRight} size='70px' className={styles.arrowRight} type='right'/>
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
