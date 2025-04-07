b4eb9ae3c23c23effcd0a7073f869b8b45256044
NikShep21 <klimenko.leva.lol2@yandex.ru>
2025-04-07 10:05:43 +0800

    changed

--- src/components/CardsSlider/cardVideo/CardVideo.tsx
+++ src/components/CardsSlider/cardVideo/CardVideo.tsx
@@ -21,6 +21,7 @@ const CardVideo = ({
       </>
     );
   }
+  const [isHovered, setIsHovered] = useState<boolean>(false)
   const [elem, setElem] = useState<VideoType | null>(
     data.find((item: VideoType) => item.type === type) || null
   );
@@ -31,10 +32,10 @@ const CardVideo = ({
   }
   const url = `https://www.youtube.com/embed/${elem.key}`;
   return (
-    <div style={{width:widthCard}} className={styles.card}>
+    <div onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)} style={{width:widthCard}} className={styles.card}>
         <div className={styles.video}>
             <img className={styles.image} src={`https://img.youtube.com/vi/${elem.key}/hqdefault.jpg`} alt="" />
-            <ButtonPlay className={styles.buttonPlay}/>
+            <ButtonPlay className={styles.buttonPlay} forceHover = {isHovered}/>
         </div>
     </div>
   );
--- src/components/ui/ButtonPlay/ButtonPlay.tsx
+++ src/components/ui/ButtonPlay/ButtonPlay.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from "./ButtonPlay.module.scss";
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     width?: number;
+    forceHover?: boolean;
 }
-const ButtonPlay = ({className="",width=60}:Props) => {
+const ButtonPlay = ({className="",width=55, forceHover=false}:Props) => {
+  console.log(forceHover)
   return (
     <div className={className}>
-        <div style={{"--size":`${width}px`} as React.CSSProperties} className={styles.buttonPlay}>
+        <div style={{"--size":`${width}px`} as React.CSSProperties} 
+        className={`${styles.buttonPlay} ${forceHover ? styles.forceHover : ""}`}>
             <div className={styles.lineLeft}></div>
             <div className={styles.lineTop}></div>
             <div className={styles.lineBottom}></div>
